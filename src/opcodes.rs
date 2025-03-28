94637ae4eb1ccb3017c25e3be782057a88987eb3
Uriel Azar <uriel.azar01@gmail.com>
2025-03-28 20:33:10 +0300

    Memory addressing

---
+++ src/opcodes.rs
@@ -0,0 +1,71 @@
+use crate::cpu::AddressingMode;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+
+pub struct OpCode {
+    pub code: u8,
+    pub mnemonic: &'static str,
+    pub bytes: u8,
+    pub cycles: u8,
+    pub mode: AddressingMode,
+}
+
+impl OpCode {
+    fn new(code: u8, mnemonic: &'static str, bytes: u8, cycles: u8, mode: AddressingMode) -> Self {
+        OpCode {
+            code: code,
+            mnemonic: mnemonic,
+            bytes: bytes,
+            cycles: cycles,
+            mode: mode,
+        }
+    }
+}
+
+static CPU_OPS_CODES: Lazy<Vec<OpCode>> = Lazy::new(|| {
+    vec![
+        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+        OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0xad, "LDA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(
+            0xbd,
+            "LDA",
+            3,
+            4, /*+1 if page crossed*/
+            AddressingMode::Absolute_X,
+        ),
+        OpCode::new(
+            0xb9,
+            "LDA",
+            3,
+            4, /*+1 if page crossed*/
+            AddressingMode::Absolute_Y,
+        ),
+        OpCode::new(0xa1, "LDA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(
+            0xb1,
+            "LDA",
+            2,
+            5, /*+1 if page crossed*/
+            AddressingMode::Indirect_Y,
+        ),
+        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0x8d, "STA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x9d, "STA", 3, 5, AddressingMode::Absolute_X),
+        OpCode::new(0x99, "STA", 3, 5, AddressingMode::Absolute_Y),
+        OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
+    ]
+});
+
+pub static CODES_MAP: Lazy<HashMap<u8, &'static OpCode>> = Lazy::new(|| {
+    CPU_OPS_CODES
+        .iter()
+        .map(|op_code| (op_code.code, op_code))
+        .collect::<HashMap<_, _>>()
+});
