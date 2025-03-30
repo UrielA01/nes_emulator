324d9f354fb228179ccb7a34d26151e90d974743
Uriel Azar <uriel.azar01@gmail.com>
2025-03-30 15:10:27 +0300

    core module

---
+++ src/cpu/core.rs
@@ -0,0 +1,132 @@
+use crate::cpu::cpu::CPU;
+use crate::cpu::opcodes;
+
+use super::{cpu::AddressingMode, flags::StatusFlags};
+
+impl CPU {
+    pub fn get_operand_address(&mut self, mode: &AddressingMode) -> u16 {
+        match mode {
+            AddressingMode::Immediate => self.program_counter,
+
+            AddressingMode::ZeroPage => self.mem_read(self.program_counter) as u16,
+
+            AddressingMode::Absolute => self.mem_read_u16(self.program_counter),
+
+            AddressingMode::ZeroPage_X => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_x) as u16;
+                addr
+            }
+            AddressingMode::ZeroPage_Y => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_y) as u16;
+                addr
+            }
+
+            AddressingMode::Absolute_X => {
+                let base = self.mem_read_u16(self.program_counter);
+                let addr = base.wrapping_add(self.register_x as u16);
+                addr
+            }
+            AddressingMode::Absolute_Y => {
+                let base = self.mem_read_u16(self.program_counter);
+                let addr = base.wrapping_add(self.register_y as u16);
+                addr
+            }
+
+            AddressingMode::Indirect => {
+                let addr = self.mem_read_u16(self.program_counter);
+
+                let lo = self.mem_read(addr);
+                let hi = self.mem_read(if addr & 0x00FF == 0x00FF {
+                    addr & 0xFF00
+                } else {
+                    addr + 1
+                });
+
+                (hi as u16) << 8 | (lo as u16)
+            }
+
+            AddressingMode::Indirect_X => {
+                let base = self.mem_read(self.program_counter);
+
+                let ptr: u8 = (base as u8).wrapping_add(self.register_x);
+                let lo = self.mem_read(ptr as u16);
+                let hi = self.mem_read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            AddressingMode::Indirect_Y => {
+                let base = self.mem_read(self.program_counter);
+
+                let lo = self.mem_read(base as u16);
+                let hi = self.mem_read((base as u8).wrapping_add(1) as u16);
+                let deref_base = (hi as u16) << 8 | (lo as u16);
+                let deref = deref_base.wrapping_add(self.register_y as u16);
+                deref
+            }
+
+            AddressingMode::NoneAddressing | _ => {
+                panic!("mode {:?} is not supported", mode);
+            }
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = StatusFlags::UNUSED | StatusFlags::BREAK;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        let start_index = 0x8000;
+        let end_index = 0x8000 + program.len();
+        let program_counter_pos = 0xFFFC;
+        self.memory[start_index..(end_index)].copy_from_slice(&program);
+        self.mem_write_u16(program_counter_pos, start_index as u16);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run()
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let code = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+            let original_program_counter = self.program_counter;
+
+            let opcode = opcodes::CODES_MAP
+                .get(&code)
+                .expect(&format!("OpCode {:x} is not recognized", code));
+
+            match code {
+                0xA9 | 0xA5 | 0xAD | 0xb5 | 0xbd | 0xb9 | 0xa1 | 0xb1 => self.lda(&opcode.mode),
+                0x69 | 0x65 | 0x75 | 0x6d | 0x7d | 0x79 | 0x61 | 0x71 => self.adc(&opcode.mode),
+
+                0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91 => self.sta(&opcode.mode),
+
+                0xAA => self.tax(),
+
+                0xe8 => self.inx(),
+
+                0x4c | 0x6c => self.jmp(&opcode.mode),
+
+                0x29 | 0x25 | 0x35 | 0x2d | 0x3d | 0x39 | 0x21 | 0x31 => self.and(&opcode.mode),
+
+                0x06 | 0x16 | 0x0e | 0x1e | 0x0a => self.asl(&opcode.mode),
+
+                0x00 => return,
+
+                _ => todo!(),
+            }
+
+            if original_program_counter == self.program_counter {
+                self.program_counter += (opcode.bytes - 1) as u16;
+            }
+        }
+    }
+}
