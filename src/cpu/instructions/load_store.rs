53a1aaa89e60b370fce3a6467ccc26f87ee9b7d7
Uriel Azar <uriel.azar01@gmail.com>
2025-03-30 15:09:52 +0300

    load_store module

---
+++ src/cpu/instructions/load_store.rs
@@ -0,0 +1,26 @@
+use crate::cpu::cpu::{AddressingMode, CPU};
+
+impl CPU {
+    pub fn lda(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    pub fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x)
+    }
+
+    pub fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x)
+    }
+
+    pub fn sta(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        self.mem_write(addr, self.register_a);
+    }
+}
