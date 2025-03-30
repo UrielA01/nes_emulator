054df51d6b5f1a6e645ed7f878c73693056ec6f2
Uriel Azar <uriel.azar01@gmail.com>
2025-03-30 14:58:15 +0300

    memory module

---
+++ src/cpu/memory.rs
@@ -0,0 +1,27 @@
+use crate::cpu::cpu::CPU;
+
+trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
+    fn mem_write(&mut self, addr: u16, data: u8);
+    fn mem_read_u16(&mut self, position: u16) -> u16 {
+        let low_bit = self.mem_read(position) as u16;
+        let high_bit = self.mem_read(position + 1) as u16;
+        (high_bit << 8) | (low_bit as u16)
+    }
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let high_bit = (data >> 8) as u8;
+        let low_bit = (data & 0xff) as u8;
+        self.mem_write(pos, low_bit);
+        self.mem_write(pos + 1, high_bit);
+    }
+}
+
+impl Mem for CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+}
