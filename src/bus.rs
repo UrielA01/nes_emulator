ed57af09b389f7a06d3082dff4aa60809419b8be
Uriel Azar <uriel.azar01@gmail.com>
2025-06-19 22:53:11 +0300

    fix: add stubs for APU mem bus

--- src/bus.rs
+++ src/bus.rs
@@ -5,6 +5,17 @@ const RAM_MIRRORS_END: u16 = 0x1FFF;
 const PPU_REGISTERS: u16 = 0x2000;
 const PPU_REGISTERS_MIRRORS_END: u16 = 0x3FFF;
 
+// APU and I/O
+pub const APU_IO_REGISTERS: u16 = 0x4000;
+pub const APU_IO_REGISTERS_END: u16 = 0x4017;
+// APU test mode and usually disabled I/O
+pub const APU_IO_DISABLED_START: u16 = 0x4018;
+pub const APU_IO_DISABLED_END: u16 = 0x401F;
+
+// Program ROM
+pub const PRG_ROM_START: u16 = 0x8000;
+pub const PRG_ROM_END: u16 = 0xFFFF;
+
 pub struct Bus {
     cpu_vram: [u8; 2048],
     rom: Rom,
@@ -37,9 +48,17 @@ impl Mem for Bus {
             }
             PPU_REGISTERS..=PPU_REGISTERS_MIRRORS_END => {
                 let _mirron_down_addr = addr & 0b00100000_00000111;
-                todo!("Support PPU");
+                // todo!("Support PPU");
+                0xff
             }
-            0x8000..=0xFFFF => self.read_prg_rom(addr),
+            PRG_ROM_START..=PRG_ROM_END => self.read_prg_rom(addr),
+
+            APU_IO_REGISTERS..=APU_IO_REGISTERS_END => {
+                // For now, stub it out safely
+                // Eventually you can emulate APU or controller
+                // todo!("Support APU");
+                0xff
+            }
 
             _ => {
                 println!("Ignoring mem access at {}", addr);
@@ -58,8 +77,13 @@ impl Mem for Bus {
                 let _mirron_down_addr = addr & 0b00100000_00000111;
                 todo!("Support PPU")
             }
-            0x8000..=0xFFFF => {
+            PRG_ROM_START..=PRG_ROM_END => {
                 panic!("Attempt to write to Cartridge ROM space")
+            }
+            APU_IO_REGISTERS..=APU_IO_REGISTERS_END => {
+                // println!("Write to APU or IO: {:04X} <- {:02X}", addr, data);
+                // Eventually implement APU and controller behavior here
+                // todo!("Support APU");
             }
             _ => {
                 println!("Ignoring mem write-access at {}", addr);
